9708fad549c86c13c3515429d08180bab95a9ded
Hanna Yatskiv <hanna.ada7@gmail.com>
2025-11-03 15:17:18 +0300

    make build

---
+++ index.3bd920bd.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("table"),r=!0,l=!1,t=void 0;try{for(var n,o=e.rows[Symbol.iterator]();!(r=(n=o.next()).done);r=!0){var a=n.value,i=a.cells;if(!(i.length<2)){var c=i[1].cloneNode(!0),f=i[i.length-1];a.insertBefore(c,f)}}}catch(e){l=!0,t=e}finally{try{r||null==o.return||o.return()}finally{if(l)throw t}}
+//# sourceMappingURL=index.3bd920bd.js.map
