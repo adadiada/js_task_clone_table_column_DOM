2f9e5381c7e8e87798e6f53b8df31d3416b5e74f
Hanna Yatskiv <hanna.ada7@gmail.com>
2025-11-03 14:24:48 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,15 +5,12 @@ const table = document.querySelector('table');
 for (const row of table.rows) {
   const cells = row.cells;
 
-  if (cells.lenght < 2) {
+  if (cells.length < 2) {
     continue;
   }
 
   const rowSecons = cells[1].cloneNode(true);
+  const rowF = cells[4];
 
-  if (cells.length <= 5) {
-    row.insertBefore(rowSecons, cells[4]);
-  } else {
-    row.appendChild(rowSecons);
-  }
+  row.insertBefore(rowSecons, rowF);
 }
