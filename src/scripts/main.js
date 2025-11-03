339a628dd26251c382f6e4114525b4e76a00b953
Hanna Yatskiv <hanna.ada7@gmail.com>
2025-11-03 14:02:22 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+for (const row of table.rows) {
+  const cells = row.cells;
+
+  if (cells.lenght < 2) {
+    continue;
+  }
+
+  const rowSecons = cells[1].cloneNode(true);
+
+  if (cells.length <= 5) {
+    row.insertBefore(rowSecons, cells[4]);
+  } else {
+    row.appendChild(rowSecons);
+  }
+}
